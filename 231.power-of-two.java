d7d4e309638483474298bef74656f7f619fbd7db
Bikramjit Saha <65163298+VickySaha107@users.noreply.github.com>
2020-05-12 19:11:14 +0530

    Power-Of-Two Solution

---
+++ 231.power-of-two.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=231 lang=java
+ *
+ * [231] Power of Two
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n<=0) 
+          return false;
+    //     long i;
+    // for( i=1;i<n;i=i*2){
+    //     if(i==n)
+    //         break;
+    // }
+    //     return i==n;
+        // long i=1;
+        // while(i<n){
+        //     i*=2;
+        // }
+        // return i==n;
+        return (Math.log10(n)/Math.log10(2))%1==0;
+    }
+}
+// @lc code=end
+
