6a458b9897b150ef7ce751d34382557da11bc029
Bikramjit Saha <65163298+VickySaha107@users.noreply.github.com>
2020-05-12 19:17:14 +0530

    Power-Of-Three

---
+++ 326.power-of-three.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=326 lang=java
+ *
+ * [326] Power of Three
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if(n<=0) 
+          return false;
+    //     long i;
+    // for( i=1;i<n;i=i*3){
+    //     if(i==n)
+    //         break;
+    // }
+    //     return i==n;
+        // long i=1;
+        // while(i<n){
+        //     i*=3;
+        // }
+        // return i==n;
+        return (Math.log10(n)/Math.log10(3))%1==0;
+    }
+}
+// @lc code=end
+
