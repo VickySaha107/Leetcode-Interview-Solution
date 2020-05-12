cd0a402dc9812e079b4656e0efffc929784ae07c
Bikramjit Saha <65163298+VickySaha107@users.noreply.github.com>
2020-05-12 19:14:51 +0530

    Valid-Anagram Solution

---
+++ 242.valid-anagram.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=242 lang=java
+ *
+ * [242] Valid Anagram
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if( s.length()!= t.length()){
+            return false; 
+         }
+           int[] HT =new int[26];
+         for(int i=0;i<s.length();i++){
+            HT[s.charAt(i) - 97]++; 
+         }
+         for(int i=0;i<t.length();i++){
+             HT[t.charAt(i) - 97]--;
+             if(HT[t.charAt(i) - 97]<0)
+                 return false;
+         }
+         return true;
+    }
+}
+// @lc code=end
+
