37ba1594a0b8be72e301be63a0da5efed4c739e4
Bikramjit Saha <65163298+VickySaha107@users.noreply.github.com>
2020-05-12 18:50:41 +0530

    Add Two Numbers Solution

---
+++ 2.add-two-numbers.java
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode id=2 lang=java
+ *
+ * [2] Add Two Numbers
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode Pointer1=l1;
+        ListNode Pointer2=l2;
+        ListNode Result=new ListNode(0);
+        ListNode ResultPointer=Result;
+        int sum=0;
+        int digitValue=0;
+        int carry=0;
+        while (Pointer1!=null||Pointer2!=null||carry!=0) {
+            ResultPointer.next=new ListNode(0);
+            ResultPointer=ResultPointer.next;
+            sum=0;
+            if(Pointer1!=null){
+                sum=sum+Pointer1.val;
+                Pointer1=Pointer1.next;
+            }
+            if(Pointer2!=null){
+                sum=sum+Pointer2.val;
+                Pointer2=Pointer2.next;
+            }
+             sum=sum+carry;
+             digitValue=sum%10;
+             carry=sum/10;
+             ResultPointer.val=digitValue;   
+            }
+            return Result.next; 
+    }
+}
+// @lc code=end
+
