5dd33d2591d0fccc90b833833be738e16c6904f0
Bikramjit Saha <65163298+VickySaha107@users.noreply.github.com>
2020-05-12 19:19:01 +0530

    Power-Of-Four Solution

---
+++ 342.power-of-four.java
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=342 lang=java
+ *
+ * [342] Power of Four
+ */
+
+// @lc code=start
+class Solution {
+    public boolean isPowerOfFour(int num) {
+        if(num<=0) 
+          return false;
+    //     long i;
+    // for( i=1;i<num;i=i*4){
+    //     if(i==num)
+    //         break;
+    // }
+    //     return i==num;
+        // long i=1;
+        // while(i<num){
+        //     i*=4;
+        // }
+        // return i==num;
+        return (Math.log10(num)/Math.log10(4))%1==0;
+    }
+}
+// @lc code=end
+
